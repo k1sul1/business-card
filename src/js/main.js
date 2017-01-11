61dc6281c42ccc03f1c08dcb3619eef97f315eec
Christian Nikkanen <christian.nikkanen@gmail.com>
2017-01-11 02:19:53 +0200

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+import Optiscroll from 'optiscroll';
+import Shake from 'shake.js';
+//import Hammer from 'hammerjs';
+
+let flipped = false;
+const vcard = document.querySelector('.vcard');
+const flipCard = () => { flipped = true; vcard.classList.add('flip') };
+const unflipCard = () => { flipped = false; vcard.classList.remove('flip') };
+const toggleFlip = () => {
+  if (flipped) {
+    return unflipCard();
+  }
+
+  return flipCard();
+}
+
+//vcard.addEventListener('mouseover', flipCard);
+//vcard.addEventListener('mouseout', unflipCard);
+
+const scrollInstances = Array.from(document.querySelectorAll('.optiscroll')).map(area => {
+  return new Optiscroll(area);
+});
+
+const shakeInstance = new Shake({
+  threshold: 15,
+  timeout: 1000
+});
+shakeInstance.start();
+
+window.addEventListener('shake', function(e) {
+  toggleFlip();
+}, false);
+
+
+Array.from(document.querySelectorAll('.toggle-flip')).map(element => {
+  element.addEventListener('click', toggleFlip);
+  return element;
+});
+
+setTimeout(() => {
+  Array.from(document.querySelectorAll('.delayed')).map(element => {
+    element.style.opacity = 1;
+    return element;
+  });
+}, 2500);
